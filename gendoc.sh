cc9921e05c9a25a1bed467a8eef8f7eb354d208c
inaddrany <inaddrany@febdc8c4-1326-0410-abd6-27cb15717650>
2008-11-13 03:09:56 +0000

    * Change gendoc.sh so that docs updates will diff properly with svn.

--- gendoc.sh
+++ gendoc.sh
@@ -2,6 +2,25 @@
 #
 # Generates documentation for the Objective-C framework.
 #
+# If we don't have a documentation directory, just generate the docs.
+if [ ! -d Documentation ]
+then
+  headerdoc2html -u -o Documentation MacFUSE.hdoc GMUserFileSystem.h GMFinderInfo.h GMResourceFork.h GMAppleDouble.h
+  gatherheaderdoc Documentation index.html
+exit 0
+fi
+
+# We do have a Documentation directory. This probably has .svn subdirs, 
+# which will mess up gatherheaderdoc. We do this ugly hack:
+if [ -d Documentation.tmp ]
+then
+  echo "Error: Documentation.tmp exists. Where is your svn directory?"
+  exit 1
+fi
+mv Documentation Documentation.tmp
 mkdir Documentation
 headerdoc2html -u -o Documentation MacFUSE.hdoc GMUserFileSystem.h GMFinderInfo.h GMResourceFork.h GMAppleDouble.h
 gatherheaderdoc Documentation index.html
+cp -r Documentation/ Documentation.tmp
+rm -rf Documentation
+mv Documentation.tmp Documentation
