b685ddac83a37a48417a4536f6339f261b1cb03b
inaddrany <inaddrany@febdc8c4-1326-0410-abd6-27cb15717650>
2008-11-13 00:41:22 +0000

    * Do not delete Documentation directory; it is ok to generate new docs over old.

--- gendoc.sh
+++ gendoc.sh
@@ -2,7 +2,6 @@
 #
 # Generates documentation for the Objective-C framework.
 #
-rm -rf Documentation
 mkdir Documentation
 headerdoc2html -u -o Documentation MacFUSE.hdoc GMUserFileSystem.h GMFinderInfo.h GMResourceFork.h GMAppleDouble.h
 gatherheaderdoc Documentation index.html
