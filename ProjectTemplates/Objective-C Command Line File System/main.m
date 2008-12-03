cb6333166f22ac2a48ba5beed114af0c927378ae
inaddrany <inaddrany@febdc8c4-1326-0410-abd6-27cb15717650>
2008-12-03 06:54:42 +0000

    * Added volume name and volume icon to Objective-C command line xcode template.

--- ProjectTemplates/Objective-C Command Line File System/main.m
+++ ProjectTemplates/Objective-C Command Line File System/main.m
@@ -9,20 +9,34 @@
 //  gcc -o "ÇPROJECTNAMEÈ" ÇPROJECTNAMEASIDENTIFIERÈ_Filesystem.m main.m 
 //      -framework MacFUSE -framework Foundation
 //
+#import <sys/stat.h>
 #import <Foundation/Foundation.h>
 #import <MacFUSE/GMUserFileSystem.h>
 #import "ÇPROJECTNAMEASIDENTIFIERÈ_Filesystem.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[], char* envp[], char** exec_path) {
   NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
   
   NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
   NSString* mountPath = [args stringForKey:@"mountPath"];
+  NSString* iconPath = [args stringForKey:@"volicon"];
   if (!mountPath || [mountPath isEqualToString:@""]) {
-    printf("\nUsage: %s -mountPath <path>\n", argv[0]);
+    printf("\nUsage: %s -mountPath <path> [-volicon <path>]\n", argv[0]);
     printf("  -mountPath: Mount point to use.\n");
     printf("Ex: %s -mountPath /Volumes/ÇPROJECTNAMEÈ\n\n", argv[0]);
     return 0;
+  }
+  if (!iconPath) {
+    // We check for a volume icon embedded as our resource fork.
+    char program_path[PATH_MAX] = { 0 };
+    if (realpath(*exec_path, program_path)) {
+      iconPath = [NSString stringWithFormat:@"%s/..namedfork/rsrc", program_path];
+      struct stat stat_buf;
+      memset(&stat_buf, 0, sizeof(stat_buf));
+      if (stat([iconPath UTF8String], &stat_buf) != 0 || stat_buf.st_size <= 0) {
+        iconPath = nil;  // We found an exec path, but the resource fork is empty.
+      }
+    }
   }
   
   ÇPROJECTNAMEASIDENTIFIERÈ_Filesystem* fs = 
@@ -31,6 +45,11 @@ int main(int argc, char* argv[]) {
                                                            isThreadSafe:NO];
   
   NSMutableArray* options = [NSMutableArray array];
+  if (iconPath != nil) {
+    NSString* volArg = [NSString stringWithFormat:@"volicon=%@", iconPath];
+    [options addObject:volArg];
+  }
+  [options addObject:@"volname=ÇPROJECTNAMEÈ"];
   // [options addObject:@"rdonly"];  <-- Uncomment to mount read-only.
   
   [userFS mountAtPath:mountPath 
