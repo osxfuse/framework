ca141f50b0f24ac4a0923c608479b1eef58ecff2
Benjamin Fleischer <fleiben@gmail.com>
2014-05-23 23:46:08 +0200

    Remove Xcode project templates

--- ProjectTemplates/Objective-C File System (Read-Only)/TemplateFS.d
+++
@@ -1,9 +0,0 @@
-#!/usr/sbin/dtrace -s
-
-#pragma D option quiet
-
-macfuse_objc*:::delegate-entry 
-/execname == "ÇPROJECTNAMEÈ"/
-{
-    printf("%-14d %s: %s\n", timestamp, probefunc, copyinstr(arg0));
-}
